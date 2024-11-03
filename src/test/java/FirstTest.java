c0213c6fc73690c62d5076484f9ea091640fe60a
gatlinglolx <shahov199@gmail.com>
2024-11-03 12:50:38 +0300

    addedGetAllLinksAsListTest

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FirstTest {
+
+    @Test
+    public void getAllLinksAsList() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+
+        driver.get("https://the-internet.herokuapp.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement link : links) {
+            String url = link.getAttribute("href");
+            System.out.println(url);
+        }
+
+        links.get(10).click();
+
+        driver.quit();
+    }
+}
